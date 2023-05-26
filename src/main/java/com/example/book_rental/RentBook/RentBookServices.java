3ffd8e4f285232729984325c8498fda6c75d5fed
bbzzyyczczeek <bbzzyyczczeek@interia.pl>
2023-05-26 18:35:39 +0200

    Penalty for late return

--- src/main/java/com/example/book_rental/RentBook/RentBookServices.java
+++ src/main/java/com/example/book_rental/RentBook/RentBookServices.java
@@ -59,7 +59,9 @@ RentBookDto rentBook(RentBookDto rentBookDto) {
         double penalty = book.getPerson().getPenalty();
         if (penalty!=0){
             System.err.print("Kara za opoznienie "+penalty);
-            //Redirected to payment
+
+          //  TODO    Redirected to payment
+
 
         } else {
             book.setLocalDate(null);
